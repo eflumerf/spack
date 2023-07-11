bc74969130434df856c75885ff672622697330c4
Chris Green <greenc@fnal.gov>
2023-07-11 13:07:02 -0500

    [boost] Add C++20 support via `cxxstd` variant

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -175,8 +175,9 @@ class Boost(Package):
             "14",
             # C++17 is not supported by Boost < 1.63.0.
             conditional("17", when="@1.63.0:"),
-            # C++20/2a is not support by Boost < 1.73.0
+            # C++20/2a is not supported by Boost < 1.73.0
             conditional("2a", when="@1.73.0:"),
+            conditional("20", when="@1.73.0:"),
         ),
         multi=False,
         description="Use the specified C++ standard when building.",
